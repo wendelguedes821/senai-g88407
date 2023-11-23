903084402706a9ca724f435da536a96299ebffe5
wendelguedes821 <wendelguedes821@gmail.com>
2023-11-22 21:22:49 -0300

    commit exercicio-02

---
+++ exercicio-02.py
@@ -0,0 +1,18 @@
+idade = int(input("Qual a sua idade? "))
+pernas = input ("Você tem pernas? ").upper()
+
+if idade <13:
+     print ("Você é uma criança.  ")
+elif idade <18:
+     print("Você é um adolescente.")
+else:
+     print ("Você é um adulto.    ")
+
+if pernas == "SIM":
+    pernas == True
+    print("Parabéns você tem pernas !! ")
+else:
+     pernas == False
+     print("CAPENGAKKKKKKK ")
+
+
